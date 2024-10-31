f6215c5518cba6d62ed359ee8b9da7f56af0fa9e
DaSASir <Sapukov1701@mail.ru>
2024-10-31 17:48:46 +0700

    создание проекта

---
+++ Object-oriented programming/laba-Array-Tychinsky/laba-Array-Tychinsky/Array.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <vector>
+#include "Array.h"
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+
---
+++ Object-oriented programming/laba-Array-Tychinsky/laba-Array-Tychinsky/Array.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+class Array {
+public:
+	//конструкторы
+	Array();
+	Array(int a[]);
+	Array(Array b);
+
+	//деструктор
+	~Array();
+
+	int AmountOfArray();//получение размера
+	void SwapArrays();//обмен содержимого с другим массивом 
+	int FindIndex();//поиск элемента
+
+	void Print();//ввод/вывод
+	void Scan();
+
+	void Sorting();//сортировка элементов
+
+	//вставка элемента по индексу
+	//удаление элемента по индексу
+	//удаление элемента по значению (первое вхождение)
+private:
+	std::vector<int> array;
+};

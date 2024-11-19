b6e4d463dd0bf97e4f3dbdfce80d0bf5fd741d3e
DaSASir <Sapukov1701@mail.ru>
2024-11-19 14:12:49 +0700

    создание массива и проверки на отсортированность

---
+++ Algorithms and data structures/laba-Bitwise_sorting_algorithm-Tychinsky/laba-Bitwise_sorting_algorithm-Tychinsky/Radix.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include "Radix.h"
+
+using std::cout;
+
+std::vector<int> CreateMassive(const int amount, const int max, const int min) {
+	std::vector<int> a;
+	for (int i = 0; i < amount; i++)
+		a.push_back(rand() % (max - min + 1) + min);
+	return a;
+}
+
+void PrintArray(const std::vector<int>& mas) {
+	for (int i = 0; i < mas.size(); i++)
+		cout << mas[i] << " ";
+}
+
+bool IsOrderly(const std::vector<int>& mas) {
+	for (int i = 0; i < mas.size() - 1; i++)
+		if (mas[i] > mas[i + 1])
+			return false;
+
+	return true;
+}
+
---
+++ Algorithms and data structures/laba-Bitwise_sorting_algorithm-Tychinsky/laba-Bitwise_sorting_algorithm-Tychinsky/Radix.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+std::vector<int> CreateMassive(const int amount, const int max, const int min);
+
+void PrintArray(const std::vector<int>& mas);
+
+bool IsOrderly(const std::vector<int>& mas);

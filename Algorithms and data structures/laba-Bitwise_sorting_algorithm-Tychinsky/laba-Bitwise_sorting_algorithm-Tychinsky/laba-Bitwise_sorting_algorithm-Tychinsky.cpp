fc09a12ea9532606db752c56699447901f14f7c7
DaSASir <Sapukov1701@mail.ru>
2024-12-08 14:53:53 +0700

    написанна лабораторная по побитовой сортировке

--- Algorithms and data structures/laba-Bitwise_sorting_algorithm-Tychinsky/laba-Bitwise_sorting_algorithm-Tychinsky/laba-Bitwise_sorting_algorithm-Tychinsky.cpp
+++ Algorithms and data structures/laba-Bitwise_sorting_algorithm-Tychinsky/laba-Bitwise_sorting_algorithm-Tychinsky/laba-Bitwise_sorting_algorithm-Tychinsky.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <time.h>
 #include "Radix.h"
 
 int main() {
@@ -19,6 +20,28 @@ int main() {
     std::cout << "\nОтсортированный массив:\n";
     Print(ms);
     std::cout << std::endl;
+
+    //2
+    for (int i = 10; i <= 1000000; i *= 10) {
+        time_t start, stop;
+        double time = 0;
+        for (int j = 0; j < 10000; j++) {
+            std::vector<int> vec = CreateMassive(10, 100 * i, -100 * i);
+
+            start = clock();
+
+            BitSorting(vec);
+
+            if (!IsOrderly(vec)) std::cout << "ИМЕЕТСЯ НЕОТСОРТИРОВАННЫЙ МАССИВ!!!";
+
+            stop = clock();
+            time += (double)(stop - start) / CLOCKS_PER_SEC;
+        }
+
+        std::cout << "Среднее время размерности " << i << " - " << time << std::endl;
+
+    }
+
 
     return 0;
 }

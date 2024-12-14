91380278b955256a8090b91032d6a2c58b25e92a
DaSASir <Sapukov1701@mail.ru>
2024-12-14 22:29:52 +0700

    пустой проект

---
+++ Algorithms and data structures/laba-Topological_sorting-Tychinsky/laba-Topological_sorting-Tychinsky/Top.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#include "..\..\..\Object-oriented programming\laba-Boolean_Matrix-Tychinsky\laba-Boolean_Matrix-Tychinsky\Matrix.h"
+#include "..\..\..\Object-oriented programming\laba-List-Tychinsky\laba-List-Tychinsky\List.h"
+
+
---
+++ Algorithms and data structures/laba-Topological_sorting-Tychinsky/laba-Topological_sorting-Tychinsky/laba-Topological_sorting-Tychinsky.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "Top.h"
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+
+
+	return 0;
+}

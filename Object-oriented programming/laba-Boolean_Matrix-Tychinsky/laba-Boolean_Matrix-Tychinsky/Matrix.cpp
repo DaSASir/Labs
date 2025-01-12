af979bc6871fb536c7f806235deaedb4bb2512d8
DaSASir <Sapukov1701@mail.ru>
2025-01-13 04:30:46 +0700

    написаны новые функции для булевой матрицы, но отсутствует подключение булеввектора

--- Object-oriented programming/laba-Boolean_Matrix-Tychinsky/laba-Boolean_Matrix-Tychinsky/Matrix.cpp
+++ Object-oriented programming/laba-Boolean_Matrix-Tychinsky/laba-Boolean_Matrix-Tychinsky/Matrix.cpp
@@ -2,198 +2,117 @@
 #include "assert.h"
 #include "Matrix.h"
 
-BoolMatrix::BoolMatrix()
-	:BoolMatrix(1,8,0) {}
+//конструкторы и деструктор
+BoolMatrix::BoolMatrix() {}
 
-BoolMatrix::BoolMatrix(const int col_count, const int row_count, const bool value)
-	:m_amount_col(col_count), m_amount_row(row_count) {
-
-	m_count_vectors = new BoolVector[m_amount_row];
-
-	BoolVector vector(m_amount_col, value);
-
-	for (int i = 0; i < m_amount_row; i++)
-		m_count_vectors[i] = vector;
-
+BoolMatrix::BoolMatrix(const int cols, const int rows) : m_cols(cols), m_rows(rows) {
+	assert(cols >= 0 && rows >= 0);
+	m_vectors = new BoolVector[m_rows];
+	for (int i = 0; i < m_rows; i++)
+		m_vectors[i] = BoolVector(m_cols);
 }
-BoolMatrix::BoolMatrix(const char** matrix, const int col_count, const int row_count)
-	:m_amount_col(col_count), m_amount_row(row_count) {
 
-	m_count_vectors = new BoolVector[m_amount_row];
-
-	for (int i = 0; i < m_amount_row; i++)
-		m_count_vectors[i] = matrix[i];
+BoolMatrix::BoolMatrix(const int cols, const int rows, const bool value) : m_cols(cols), m_rows(rows) {
+	m_vectors = new BoolVector[m_rows];
+	for (int i = 0; i < m_rows; i++)
+		m_vectors[i] = BoolVector(m_cols, value);
 }
-BoolMatrix::BoolMatrix(const BoolMatrix& other)
-	:m_amount_col(other.m_amount_col), m_amount_row(other.m_amount_row) {
 
-	m_count_vectors = new BoolVector[m_amount_row];
+BoolMatrix::BoolMatrix(const char** matrix, const int cols, const int rows) : m_cols(cols), m_rows(rows) {
+	m_vectors = new BoolVector[m_rows];
+	for (int i = 0; i < m_rows; i++) 
+		m_vectors[i] = BoolVector(matrix[i], cols);
+}
 
-	for (int i = 0; i < m_amount_row; i++)
-		m_count_vectors[i] = other.m_count_vectors[i];
+BoolMatrix::BoolMatrix(const BoolMatrix& other) : m_cols(other.m_cols), m_rows(other.m_rows) {
+	m_vectors = new BoolVector[m_rows];
+	for (int i = 0; i < m_rows; i++)
+		m_vectors[i] = other.m_vectors[i];
 }
 
 BoolMatrix::~BoolMatrix() {
-	delete[] m_count_vectors;
+	delete[] m_vectors;
 }
 
-int BoolMatrix::GetAmountOfRows() const {
-	return m_amount_row;
+//кол-во строк и столбцов и обмен матриц
+int BoolMatrix :: AmountOfRows() const {
+	return m_rows;
 }
-int BoolMatrix::GetAmountOfCols() const {
-	return m_amount_col;
+
+int BoolMatrix::AmountOfCols() const {
+	return m_cols;
 }
 
 void BoolMatrix::Swap(BoolMatrix& other) {
 	using std::swap;
-	swap(m_amount_col, other.m_amount_col);
-	swap(m_amount_row, other.m_amount_row);
-	swap(m_count_vectors, other.m_count_vectors);
+	swap(m_cols, other.m_cols);
+	swap(m_rows, other.m_rows);
+	swap(m_vectors, other.m_vectors);
 }
 
-void BoolMatrix::Print() const{
-	for (int i = 0; i < m_amount_row; i++) {
-		m_count_vectors[i].Print();
-		std::cout << std::endl;
-	}
+//ввод и вывод
+void BoolMatrix::Print() const {
+	for (int i = 0; i < m_rows; i++) 
+		m_vectors[i].Print();
+	std::cout << std::endl;
 }
 
-int BoolMatrix::Weight() const{
+void BoolMatrix::Scan() {
+	for (int i = 0; i < m_rows; i++)
+		m_vectors[i].Scan();
+}
+
+//конъюкция и дизъюнкция всех строк
+BoolVector BoolMatrix::ConjunctionOfRows() {
+	BoolVector back(m_vectors[0]);
+	for (int i = 1; i < m_rows; i++) 
+		back &= m_vectors[i];
+	return back;
+}
+
+BoolVector BoolMatrix::DisjunctionOfRows() {
+	BoolVector back(m_vectors[0]);
+	for (int i = 1; i < m_rows; i++)
+		back |= m_vectors[i];
+	return back;
+}
+
+//вес матрицы и строки
+int BoolMatrix::Weight() const {
 	int weight = 0;
-
-	for (int i = 0; i < m_amount_row; i++)
-		weight += m_count_vectors[i].WeightVector();
-
+	for (int i = 0; i < m_cols; i++)
+		weight += m_vectors[i].WeightVector();
 	return weight;
 }
 
-BoolVector BoolMatrix::ConjunctionOfRows() {
-	BoolVector back(m_count_vectors[0]);
-
-	for (int i = 1; i < m_amount_row; i++)
-		back &= m_count_vectors[i];
-
-	return back;
-}
-BoolVector BoolMatrix::DisjunctionOfRows() {
-	BoolVector back(m_count_vectors[0]);
-
-	for (int i = 1; i < m_amount_row; i++)
-		back |= m_count_vectors[i];
-
-	return back;
+int BoolMatrix::Weight(const int row) const {
+	assert(row >= 0 && row < m_rows);
+	return m_vectors[row].WeightVector();
 }
 
-int BoolMatrix::Weight(const int index_row) const{
-	return m_count_vectors[index_row].WeightVector();
+//инверсии
+void BoolMatrix::Inversion(const int col, const int row) {
+	assert(col >= 0 && col < m_cols && row < m_rows && row >= 0);
+	m_vectors[row].Inversion(col);
 }
 
-void BoolMatrix::Inversion(const int index_i, const int index_j) {
-	m_count_vectors[index_j].Inversion(index_i);
-}
-void BoolMatrix::Inversion(const int index_i, const int index_j, const int k) {
-	assert(index_i + k < m_amount_col);
-	for(int i = 0; i < k; i++)
-		m_count_vectors[index_j].Inversion(index_i);
+void BoolMatrix::Inversion(const int from, const int row, const int to) {
+	assert(row >= 0 && row < m_rows);
+	for (int i = from; i <= to; i++)
+		m_vectors[row].Inversion(i);
 }
 
-void BoolMatrix::Set(const bool value, const int index_i, const int index_j) {
-	m_count_vectors[index_j].Set(value, index_i);
-}
-void BoolMatrix::Set(const bool value, const int index_i, const int index_j, const int k) {
-	assert(index_i + k < m_amount_col);
-	for (int i = 0; i < k; i++)
-		m_count_vectors[index_j].Set(value, index_i);
+//вставки битов
+void BoolMatrix::Set(const bool value, const int col, const int row) {
+	assert(row >= 0 && row < m_rows);
+	m_vectors[row].Set(value, col);
 }
 
-BoolMatrix& BoolMatrix::operator = (const BoolMatrix& other) {
-	if (this == &other) return *this;
-
-	m_amount_row = other.m_amount_row;
-	m_amount_col = other.m_amount_col;
-	delete[] m_count_vectors;
-	m_count_vectors = new BoolVector[m_amount_row];
-
-	for (int i = 0; i < m_amount_row; i++)
-		m_count_vectors[i] = other.m_count_vectors[i];
-
-	return *this;
+void BoolMatrix::Set(const bool value, const int from, const int row, const int to) {
+	assert(row >= 0 && row < m_rows);
+	m_vectors[row].Set(value, from, to);
 }
 
-BoolVector& BoolMatrix::operator [] (const int index) {
-	assert(index >= 0 && index < m_amount_row);
-	return m_count_vectors[index];
-}
+//перегрузки
 
-const BoolVector& BoolMatrix::operator [] (const int index) const {
-	assert(index >= 0 && index < m_amount_row);
-	return m_count_vectors[index];
-}
-
-BoolMatrix BoolMatrix::operator & (const BoolMatrix& other) const {
-	BoolMatrix back(*this);
-
-	for (int i = 0; i < m_amount_row; i++)
-		back.m_count_vectors[i] &= other.m_count_vectors[i];
-
-	return back;
-}
-BoolMatrix& BoolMatrix::operator &= (const BoolMatrix& other) {
-	(*this) = *this & other;
-	return *this;
-}
-
-BoolMatrix BoolMatrix::operator | (const BoolMatrix& other) const {
-	BoolMatrix back(*this);
-
-	for (int i = 0; i < m_amount_row; i++)
-		back.m_count_vectors[i] |= other.m_count_vectors[i];
-
-	return back;
-}
-BoolMatrix& BoolMatrix::operator |= (const BoolMatrix& other) {
-	(*this) = *this | other;
-	return *this;
-}
-
-BoolMatrix BoolMatrix::operator ^ (const BoolMatrix& other) const {
-	BoolMatrix back(*this);
-
-	for (int i = 0; i < m_amount_row; i++)
-		back.m_count_vectors[i] ^= other.m_count_vectors[i];
-
-	return back;
-}
-BoolMatrix& BoolMatrix::operator ^= (const BoolMatrix& other) {
-	(*this) = *this ^ other;
-	return *this;
-}
-
-BoolMatrix BoolMatrix::operator ~ () const {
-	BoolMatrix back(*this);
-
-	for (int i = 0; i < m_amount_row; i++) 
-		back.m_count_vectors[i].Inversion();
-
-	return back;
-}
-
-std::istream& operator >> (std::istream& stream, BoolMatrix& other) {
-
-	for (int i = 0; i < other.GetAmountOfCols(); i++) 
-		stream >> other[i];
-
-	return stream;
-}
-
-std::ostream& operator << (std::ostream& stream, const BoolMatrix& other) {
-	for (int i = 0; i < other.GetAmountOfCols(); ++i) {
-		stream << "[ ";
-		for (int j = 0; j < other.GetAmountOfRows(); ++j) 
-			stream << other[i][j] << ((j + 1) < other.GetAmountOfRows() ? " " : "");
-		
-		stream << "]\n";
-	}
-
-	return stream;
-}
+//потоковый ввод и вывод
--- Object-oriented programming/laba-Boolean_Matrix-Tychinsky/laba-Boolean_Matrix-Tychinsky/Matrix.h
+++ Object-oriented programming/laba-Boolean_Matrix-Tychinsky/laba-Boolean_Matrix-Tychinsky/Matrix.h
@@ -1,60 +1,52 @@
 ﻿#pragma once
 #include "../../laba-Boolean_vector-Tychinsky/laba-Boolean_vector-Tychinsky/Vector.h"
+//#include "C:\GitHub_Repositories\Labs\Object-oriented programming\laba-Boolean_vector-Tychinsky\laba-Boolean_vector-Tychinsky/Vector.h"
 
 class BoolMatrix {
 public:
-	// конструкторы (по умолчанию, с параметрами (количество строк/столбцов и значения разрядов), конструктор из матрицы char, конструктор копирования);
 	BoolMatrix();
-	BoolMatrix(const int col_count, const int row_count, const bool value);
-	BoolMatrix(const char** matrix, const int col_count, const int row_count);
+	BoolMatrix(const int cols, const int rows);
+	BoolMatrix(const int cols, const int rows, const bool value);
+	BoolMatrix(const char** matrix, const int cols, const int rows);
 	BoolMatrix(const BoolMatrix& other);
-	// деструктор;
 	~BoolMatrix();
-	//получение числа строк и столбцов матрицы;
-	int GetAmountOfRows() const;
-	int GetAmountOfCols() const;
-	//обмен содержимого с другой матрицей (swap);
+
+	int AmountOfRows() const;
+	int AmountOfCols() const;
+
 	void Swap(BoolMatrix& other);
-	//ввод/вывод в консоль (потоковый);
+
 	void Print() const;
-	//вес матрицы (количество единичных компонент);
-	int Weight() const;
-	//конъюнкция всех строк (возвращает булев вектор);
+	void Scan();
+
 	BoolVector ConjunctionOfRows();
-	//дизъюнкция всех строк (возвращает булев вектор);
 	BoolVector DisjunctionOfRows();
-	//вес j-ой строки;
-	int Weight(const int index_row) const;
-	//инверсия в i-ой компоненты j-ой строки;
-	void Inversion(const int index_i, const int index_j);
-	//инверсия k компонент j-ой строки, начиная с i-ой компоненты;
-	void Inversion(const int index_i, const int index_j, const int k);
-	//установка в 0/1 i-ой компоненты j-ой строки;
-	void Set(const bool value, const int index_i, const int index_j);
-	//установка в 0/1 k компонент j-ой строки, начиная с i-ой компоненты;
+
+	int Weight() const;
+	int Weight(const int row) const;
+
+	void Inversion(const int col, const int row);
+	void Inversion(const int from, const int row, const int to);
+
+	void Set(const bool value, const int col, const int row);
 	void Set(const bool value, const int index_i, const int index_j, const int k);
 
-	//присваивание (=);
 	BoolMatrix& operator = (const BoolMatrix& other);
-	//получение строки([]);
 	BoolVector& operator [] (const int index);
 	const BoolVector& operator [] (const int index) const;
-	//построчное побитовое умножение(&, &=);
 	BoolMatrix operator & (const BoolMatrix& other) const;
 	BoolMatrix& operator &= (const BoolMatrix& other);
-	//построчное побитовое сложение(| , |=);
 	BoolMatrix operator | (const BoolMatrix& other) const;
 	BoolMatrix& operator |= (const BoolMatrix& other);
-	//построчное побитовое исключающее ИЛИ(^, ^=);
 	BoolMatrix operator ^ (const BoolMatrix& other) const;
 	BoolMatrix& operator ^= (const BoolMatrix& other);
-	//построчная побитовая инверсия(~).
 	BoolMatrix operator ~ () const;
 
 private:
-	BoolVector* m_count_vectors = nullptr;
-	int m_amount_row, m_amount_col;
+	BoolVector* m_vectors = nullptr;
+	int m_rows = 0, 
+		m_cols = 0;
 };
 
-std::istream& operator >> (std::istream& stream, BoolMatrix& other);
-std::ostream& operator << (std::ostream& stream, const BoolMatrix& other);
+std::ostream& operator << (std::ostream& stream, const BoolMatrix& bm);
+std::istream& operator >> (std::istream& stream, BoolMatrix& bm);
